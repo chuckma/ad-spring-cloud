56d5f88db0bc4a1f746165beeae90e81d4b87e4f
chuckma <robbincen@163.com>
2019-01-29 16:15:31 +0800

    adplan index

---
+++ lucas-ad-service/ad-search/src/main/java/cn/lucas/ad/index/IndexAware.java
@@ -0,0 +1,15 @@
+package cn.lucas.ad.index;
+
+/**
+ * @author mcg
+ */
+public interface IndexAware<K, V> {
+
+    V get(K key);
+
+    void add(K key, V value);
+
+    void update(K key, V value);
+
+    void delete(K key, V value);
+}
---
+++ lucas-ad-service/ad-search/src/main/java/cn/lucas/ad/index/adplan/AdPlanObject.java
@@ -0,0 +1,56 @@
+package cn.lucas.ad.index.adplan;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author mcg
+ * <p>
+ * adplan 索引对象
+ * 包含 adplan 的基本信息
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdPlanObject {
+
+    /**
+     * 只需要给检索时需要的字段添加索引
+     * 我们会把数据表的实体类转换成一个索引对象
+     */
+
+    private Long planId;
+    private Long userId;
+    private Integer planStatus;
+    private Date startDate;
+    private Date endDate;
+
+
+    /**
+     * 更新索引
+     */
+    public void update(AdPlanObject newObject) {
+        if (null != newObject.getPlanId()) {
+            this.planId = newObject.getPlanId();
+        }
+
+        if (null != newObject.getUserId()) {
+            this.userId = newObject.getUserId();
+        }
+
+        if (null != newObject.planStatus) {
+            this.planStatus = newObject.getPlanStatus();
+        }
+
+        if (null != newObject.getStartDate()) {
+            this.startDate = newObject.getStartDate();
+        }
+
+        if (null != newObject.getEndDate()) {
+            this.endDate = newObject.getEndDate();
+        }
+    }
+}
