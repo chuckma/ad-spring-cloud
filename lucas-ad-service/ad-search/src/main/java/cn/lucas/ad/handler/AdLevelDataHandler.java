f5ca5791f3c81883ee37b06db666a53dcff9a34c
chuckma <robbincen@163.com>
2019-02-15 17:21:34 +0800

    index

---
+++ lucas-ad-service/ad-search/src/main/java/cn/lucas/ad/handler/AdLevelDataHandler.java
@@ -0,0 +1,14 @@
+package cn.lucas.ad.handler;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Administrator
+ *
+ * 1. 索引之间存在着层级的划分，也就是依赖关系的划分
+ * 2.
+ */
+@Slf4j
+public class AdLevelDataHandler {
+
+}
