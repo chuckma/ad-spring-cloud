ea9d6c7a57b8feebd4c625469a1dc00697bba13b
lucas ma <robbincen@163.com>
2019-02-16 14:56:02 +0800

    level index

--- lucas-ad-service/ad-search/src/main/java/cn/lucas/ad/handler/AdLevelDataHandler.java
+++ lucas-ad-service/ad-search/src/main/java/cn/lucas/ad/handler/AdLevelDataHandler.java
@@ -1,8 +1,27 @@
 package cn.lucas.ad.handler;
 
+import cn.lucas.ad.dump.table.*;
+import cn.lucas.ad.index.DataTable;
 import cn.lucas.ad.index.IndexAware;
+import cn.lucas.ad.index.adplan.AdPlanIndex;
+import cn.lucas.ad.index.adplan.AdPlanObject;
+import cn.lucas.ad.index.adunit.AdUnitIndex;
+import cn.lucas.ad.index.adunit.AdUnitObject;
+import cn.lucas.ad.index.creative.CreativeIndex;
+import cn.lucas.ad.index.creative.CreativeObject;
+import cn.lucas.ad.index.creativeunit.CreativeUnitIndex;
+import cn.lucas.ad.index.creativeunit.CreativeUnitObject;
+import cn.lucas.ad.index.district.UnitDistrictIndex;
+import cn.lucas.ad.index.interest.UnitItIndex;
+import cn.lucas.ad.index.keyword.UnitKeywordIndex;
 import cn.lucas.ad.mysql.constant.OpType;
+import cn.lucas.ad.utils.CommonUtils;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Administrator
@@ -13,6 +32,193 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AdLevelDataHandler {
 
+
+    public static void handlerLevel2(AdPlanTable planTable,OpType type){
+
+        AdPlanObject adPlanObject = new AdPlanObject(
+                planTable.getId(),
+                planTable.getUserId(),
+                planTable.getPlanStatus(),
+                planTable.getStartDate(),
+                planTable.getEndDate()
+        );
+        handlerBinlogEvent(
+                DataTable.of(AdPlanIndex.class),
+                adPlanObject.getPlanId(),
+                adPlanObject,
+                type
+        );
+
+    }
+    public static void handleLevel2(AdCreativeTable creativeTable,
+                                    OpType type) {
+
+        CreativeObject creativeObject = new CreativeObject(
+                creativeTable.getAdId(),
+                creativeTable.getName(),
+                creativeTable.getType(),
+                creativeTable.getMaterialType(),
+                creativeTable.getHeight(),
+                creativeTable.getWidth(),
+                creativeTable.getAuditStatus(),
+                creativeTable.getAdUrl()
+        );
+        handlerBinlogEvent(
+                DataTable.of(CreativeIndex.class),
+                creativeObject.getAdId(),
+                creativeObject,
+                type
+        );
+    }
+
+
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
+
+        AdPlanObject adPlanObject = DataTable.of(
+                AdPlanIndex.class
+        ).get(unitTable.getPlanId());
+        if (null == adPlanObject) {
+            log.error("handleLevel3 found AdPlanObject error: {}",
+                    unitTable.getPlanId());
+            return;
+        }
+
+        AdUnitObject unitObject = new AdUnitObject(
+                unitTable.getUnitId(),
+                unitTable.getUnitStatus(),
+                unitTable.getPositionType(),
+                unitTable.getPlanId(),
+                adPlanObject
+        );
+
+        handlerBinlogEvent(
+                DataTable.of(AdUnitIndex.class),
+                unitTable.getUnitId(),
+                unitObject,
+                type
+        );
+    }
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("CreativeUnitIndex not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(creativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(
+                CreativeIndex.class
+        ).get(creativeUnitTable.getAdId());
+
+        if (null == unitObject || null == creativeObject) {
+            log.error("AdCreativeUnitTable index error: {}",
+                    JSON.toJSONString(creativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(
+                creativeUnitTable.getAdId(),
+                creativeUnitTable.getUnitId()
+        );
+        handlerBinlogEvent(
+                DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(
+                        creativeUnitObject.getAdId().toString(),
+                        creativeUnitObject.getUnitId().toString()
+                ),
+                creativeUnitObject,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitDistrictTable unitDistrictTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("district index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(unitDistrictTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitDistrictTable index error: {}",
+                    unitDistrictTable.getUnitId());
+            return;
+        }
+
+        String key = CommonUtils.stringConcat(
+                unitDistrictTable.getProvince(),
+                unitDistrictTable.getCity()
+        );
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(unitDistrictTable.getUnitId())
+        );
+        handlerBinlogEvent(
+                DataTable.of(UnitDistrictIndex.class),
+                key, value,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitItTable unitItTable, OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("it index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(unitItTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitItTable index error: {}",
+                    unitItTable.getUnitId());
+            return;
+        }
+
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(unitItTable.getUnitId())
+        );
+        handlerBinlogEvent(
+                DataTable.of(UnitItIndex.class),
+                unitItTable.getItTag(),
+                value,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitKeywordTable keywordTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("keyword index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(keywordTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitKeywordTable index error: {}",
+                    keywordTable.getUnitId());
+            return;
+        }
+
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(keywordTable.getUnitId())
+        );
+        handlerBinlogEvent(
+                DataTable.of(UnitKeywordIndex.class),
+                keywordTable.getKeyword(),
+                value,
+                type
+        );
+    }
     private static <K, V> void handlerBinlogEvent(
             IndexAware<K, V> index,
             K key,
--- lucas-ad-service/ad-search/src/main/java/cn/lucas/ad/utils/CommonUtils.java
+++ lucas-ad-service/ad-search/src/main/java/cn/lucas/ad/utils/CommonUtils.java
@@ -1,5 +1,13 @@
 package cn.lucas.ad.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -7,12 +15,43 @@ import java.util.function.Supplier;
  * @Author mcg
  * @Date 2019/1/29 21:45
  **/
-
+@Slf4j
 public class CommonUtils {
 
 
     public static <K, V> V getOrCreate(K key, Map<K, V> map, Supplier<V> factory) {
 
         return map.computeIfAbsent(key, k -> factory.get());
+    }
+
+    public static String stringConcat(String... args) {
+
+        StringBuilder result = new StringBuilder();
+        for (String arg : args) {
+            result.append(arg);
+            result.append("-");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    // Tue Jan 01 08:00:00 CST 2019
+    public static Date parseStringDate(String dateString) {
+
+        try {
+
+            DateFormat dateFormat = new SimpleDateFormat(
+                    "EEE MMM dd HH:mm:ss zzz yyyy",
+                    Locale.US
+            );
+            return DateUtils.addHours(
+                    dateFormat.parse(dateString),
+                    -8
+            );
+
+        } catch (ParseException ex) {
+            log.error("parseStringDate error: {}", dateString);
+            return null;
+        }
     }
 }
